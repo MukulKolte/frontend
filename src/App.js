1a591cce5878391f5eb8c922ad1847c6a0eac89e
MukulKolte <mukulkolte@gmail.com>
2023-12-17 12:24:04 +0530

    adding login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
-import Navbar from './Components/Navbar';
-import Body from './Components/Body';
-import Foooter from './Components/Footer';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Loginpage from './Components/Loginpage';
+import Homepage from './Components/Homepage';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Body />
-      <Foooter />
-      {/* <Loginpage /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/login' element={<Loginpage />} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
