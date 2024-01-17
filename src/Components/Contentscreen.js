3b724bf4111a76011407de5f4ea78c5485b06855
MukulKolte <mukulkolte@gmail.com>
2024-01-17 20:57:12 +0530

    solved problem

--- src/Components/Contentscreen.js
+++ src/Components/Contentscreen.js
@@ -22,35 +22,119 @@ function Contentscreen(props) {
   // const zoomPluginInstance = zoomPlugin();
   const zoomPluginInstance = zoomPlugin();
   const { CurrentScale, ZoomIn, ZoomOut } = zoomPluginInstance;
-  
+
 
   function closeQueWindow() {
     document.getElementById("que_paper_screen").classList.add('hidden');
 
   }
 
-  
 
+
+
+  // return (
+  //   <>
+  //     <div className='h-[5%] xl:mr-[23%] lg:mr-[23%] md:mr-[10%] sm:mr-[9%] mr-[8%] float-right'><button className='bg-white rounded-full scale-130 hover:scale-150 duration-300' onClick={closeQueWindow}><img className='text-white' width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/multiply.png" alt="multiply" /></button></div>
+  //     <div>
+  //       <div>
+
+  // <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+  //   <div
+
+  //     className='max-w-[50%] mt-[5%] ml-[25%] mt-[25px] text-center h-[1000px] overflow-scroll'
+  //   >
+
+  //     <Viewer className='' fileUrl={'https://padhai-planet.s3.ap-south-1.amazonaws.com/10/question_paper.pdf'} plugins={[zoomPluginInstance]} />
+  //   </div>
+  // </Worker>
+  //       </div>
+  //     </div>
+
+  // const zoomPluginInstance = zoomPlugin();
+  // const {CurrentScale, ZoomIn, ZoomOut} = zoomPluginInstance;
 
   return (
-    <>
-      <div className='h-[5%] xl:mr-[23%] lg:mr-[23%] md:mr-[10%] sm:mr-[9%] mr-[8%] float-right'><button className='bg-white rounded-full scale-130 hover:scale-150 duration-300' onClick={closeQueWindow}><img className='text-white' width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/multiply.png" alt="multiply" /></button></div>
-      <div>
-        <div>
-
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-            <div
-
-              className='max-w-[50%] mt-[5%] ml-[25%] mt-[25px] text-center h-[1000px] overflow-scroll'
-            >
-
-              <Viewer className='' fileUrl={'https://padhai-planet.s3.ap-south-1.amazonaws.com/10/question_paper.pdf'} plugins={[zoomPluginInstance]} />
-            </div>
-          </Worker>
+    <div
+      style={{
+        border: '1px solid rgba(0, 0, 0, 0.3)',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+      }}
+    >
+      <div
+        style={{
+          alignItems: 'center',
+          backgroundColor: '#eeeeee',
+          borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '4px',
+        }}
+      >
+        <div style={{ padding: '0px 2px' }}>
+          <ZoomOut>
+            {(RenderZoomOutProps) => (
+              <button
+                style={{
+                  backgroundColor: '#357edd',
+                  border: 'none',
+                  borderRadius: '4px',
+                  color: '#ffffff',
+                  cursor: 'pointer',
+                  padding: '8px',
+                }}
+                onClick={RenderZoomOutProps.onClick}
+              >
+                Zoom out
+              </button>
+            )}
+          </ZoomOut>
+        </div>
+        <div style={{ padding: '0px 2px' }}>
+          <CurrentScale>
+            {(RenderCurrentScaleProps) => <>{`${Math.round(RenderCurrentScaleProps.scale * 100)}%`}</>}
+          </CurrentScale>
+        </div>
+        <div style={{ padding: '0px 2px' }}>
+          <ZoomIn>
+            {(RenderZoomInProps) => (
+              <button
+                style={{
+                  backgroundColor: '#357edd',
+                  border: 'none',
+                  borderRadius: '4px',
+                  color: '#ffffff',
+                  cursor: 'pointer',
+                  padding: '8px',
+                }}
+                onClick={RenderZoomInProps.onClick}
+              >
+                Zoom in
+              </button>
+            )}
+          </ZoomIn>
         </div>
       </div>
-    </>
-  )
+      <div
+        style={{
+          flex: 1,
+          overflow: 'hidden',
+        }}
+      >
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+          <div
+
+            className='max-w-[50%] mt-[5%] ml-[25%] mt-[25px] text-center h-[1000px] overflow-scroll'
+          >
+
+            <Viewer className='' fileUrl={'https://padhai-planet.s3.ap-south-1.amazonaws.com/10/question_paper.pdf'} plugins={[zoomPluginInstance]} />
+          </div>
+        </Worker>
+      </div>
+    </div>
+  );
+
 
 }
 
